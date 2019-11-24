9bab707f520f748389dde01d3fd7528414b88dbf
evert <evert@gmail.com>
2019-11-24 11:34:35 -0600

    Proyecto inicial

---
+++ src/zlink/Zlink.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zlink;
+
+import zlink.form.InicialForm;
+
+/**
+ *
+ * @author evert
+ */
+public class Zlink {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new InicialForm().setVisible(true);
+    }
+    
+}
